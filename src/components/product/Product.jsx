7f83059496801d37045199df0d689e217a939062
Arvind Gupta <110850319+ArvindGupta493@users.noreply.github.com>
2024-12-04 16:12:14 +0530

    Update Product.jsx

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css"; // Leaflet CSS
+// import "leaflet/dist/leaflet.css"; // Leaflet CSS
 import L from "leaflet"; // Leaflet core
 import Header from "../header/header";
 import Footer from "../footer/Footer";
